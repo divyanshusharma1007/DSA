1ff528414b0773ffdaa1048dfc4c74e7804ba1fb
Divyanshu Sharma <1007divyanshu@gmail.com>
2026-05-16 22:48:22 +0530

    question 4 sql

--- SQL/easy/question2.sql
+++ SQL/easy/question2.sql
@@ -1,4 +1,4 @@
 
--- #Query the total population of all cities in CITY where District is California.
+-- #Query the avg population of all cities in CITY where District is California.
 
 SELECT AVG(POPULATION) FROM CITY WHERE DISTRICT='California';
